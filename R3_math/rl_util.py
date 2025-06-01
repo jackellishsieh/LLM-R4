dc2a6686f727a0b97d26c21b8362e8abe152a22a
Jack Hsieh <jackellishsieh@gmail.com>
2025-06-01 21:38:23 +0000

    Update rl_util cot info, changing keys and using deepseek format

--- R3_math/rl_util.py
+++ R3_math/rl_util.py
@@ -1,73 +1,79 @@
+import re
 import torch
 from typing import Callable, TypedDict
 import constants
 
+
 class CotInfo(TypedDict):
     """A TypedDict to hold information related to Chain of Thought question processing and evaluation."""
-    instruction: str        # the prompt instruction, to be followed by the question
-    cot_trigger: str        # the reponse trigger, to be followed by the generated CoT
-    answer_trigger: str     # the answer trigger, to be followed by the final answer
-    post_process_final_answer_fn_mapper: dict[str, Callable[[str], float]]      # post-process final answers (by removing spaces and commas)
-    post_process_completed_question_answer_fn_mapper: dict[tuple[str, str], Callable[[str], float]] # post-process completed question answers (by extracting the answer from the generated CoT)
-    compare_answer_fn_mapper: dict[str, Callable[[float, float], bool]] # compare the extracted answer with the target answer
+
+    question_to_prompt: Callable[
+        [str], str
+    ]  # function to convert a question to a prompt
+    cot_to_answer: Callable[
+        [str], float
+    ]  # function to convert a generated Chain of Thought (CoT) to a final answer
+    compare_answer: Callable[
+        [float, float], bool
+    ]  # function to compare the extracted answer with the target answer
+
+    # instruction: str        # the prompt instruction, to be followed by the question
+    # cot_trigger: str        # the reponse trigger, to be followed by the generated CoT
+    # answer_trigger: str     # the answer trigger, to be followed by the final answer
+    # post_process_final_answer_fn_mapper: dict[str, Callable[[str], float]]      # post-process final answers (by removing spaces and commas)
+    # post_process_completed_question_answer_fn_mapper: dict[tuple[str, str], Callable[[str], float]] # post-process completed question answers (by extracting the answer from the generated CoT)
+    # compare_answer_fn_mapper: dict[str, Callable[[float, float], bool]] # compare the extracted answer with the target answer
+
+
+def deepseek_question_to_prompt(question: str) -> str:
+    """
+    Convert a question to a prompt using the DeepSeek r1-zero format.
+    """
+    return constants.gsm8k_instruction.replace("{question}", question)
+
+
+def deepseek_cot_to_answer(cot: str) -> float:
+    """
+    Convert a generated Chain of Thought (CoT) to a final answer using the DeepSeek r1-zero format.
+    """
+    # Identify the last occurence of <answer>...</answer> in the CoT
+    answer_matches = re.findall(
+        r"<answer>(.*?)</answer>", cot
+    )  # really, there should only be one match
+    if not answer_matches:
+        return None
+
+    last_answer_match = answer_matches[-1]  # take the last match if it exists
+    last_extracted_answer = float(
+        last_answer_match.replace(",", "").strip()
+    )  # remove commas and spaces, then convert to float
+    return last_extracted_answer
+
+
+def gsm8k_compare_answer(
+    extracted_ans: float, target_answer: float, tolerance=1e-2
+) -> bool:
+    """
+    Compare the extracted answer with the target answer for the GSM8K dataset.
+    Returns True if the absolute difference is within a small tolerance, otherwise False.
+    """
+    return abs(extracted_ans - target_answer) <= tolerance
+
 
 def prepare_cot_info(src_name):
     """
     Given a source name, prepare the instruction, COT trigger, answer trigger, and post-processing functions.
     This is a function only of the dataset
     """
-    assert src_name in ["gsm8k", "svamp"], f"Source name ({src_name}) must be either 'gsm8k' or 'svamp'."
-
-    # default for common datasets
-    instruction = "Question:\n"
-    cot_trigger = "\nAnswer reasoning:\n"
-    answer_trigger = "\nTherefore, the answer is: "
-
-    if src_name in ["gsm8k", "svamp"]:
-        # over-write for the simple datasets
-        # instruction = "Below is an instruction that describes a task. Write a response that appropriately completes the request.\n\n### Instruction:\n"
-        instruction = constants.gsm8k_instruction
-        
-        cot_trigger = "\n\n### Response:"
-        answer_trigger = "\n####"
-
-    # post process final answers by removing commas and spaces
-    post_process_final_answer_fn_mapper = {
-        "gsm8k": lambda x: float(x.replace(",", "").strip()),
-        "svamp": lambda x: float(x.replace(",", "").strip()),
-    }
-
-    # run code to extract answer preceding ####
-    post_process_completed_question_answer_fn_mapper = {
-        # ('python', 'gsm8k'): lambda completed_question_answer: float(run_python_code(code_gen=completed_question_answer.split(cot_trigger)[-1].strip())),
-        # ('python', 'svamp'): lambda completed_question_answer: float(run_python_code(code_gen=completed_question_answer.split(cot_trigger)[-1].strip())),
-        ("nl", "gsm8k"): lambda completed_question_answer: float(
-            completed_question_answer.split(cot_trigger)[-1]
-            .split(answer_trigger)[-1]
-            .strip()
-        ),
-        ("nl", "svamp"): lambda completed_question_answer: float(
-            completed_question_answer.split(cot_trigger)[-1]
-            .split(answer_trigger)[-1]
-            .strip()
-        ),
-    }
-
-    # compare for equality, essentially
-    compare_answer_fn_mapper = {
-        "gsm8k": lambda extracted_ans, target_answer: abs(extracted_ans - target_answer)
-        <= 1e-2,
-        "svamp": lambda extracted_ans, target_answer: abs(extracted_ans - target_answer)
-        <= 1e-2,
-    }
+    assert src_name in [
+        "gsm8k",
+        "svamp",
+    ], f"Source name ({src_name}) must be either 'gsm8k' or 'svamp'."
 
     return {
-        "instruction": instruction,
-        "cot_trigger": cot_trigger,
-        "answer_trigger": answer_trigger,
-        "post_process_final_answer_fn_mapper": post_process_final_answer_fn_mapper,
-        "post_process_completed_question_answer_fn_mapper": post_process_completed_question_answer_fn_mapper,
-        "compare_answer_fn_mapper": compare_answer_fn_mapper,
+        "question_to_prompt": deepseek_question_to_prompt,
+        "cot_to_answer": deepseek_cot_to_answer,
+        "compare_answer": gsm8k_compare_answer,
     }
 
 
