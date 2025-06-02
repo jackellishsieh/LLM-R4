53f94fb9a23b99ff9c1fbc1d15e394eb2a9a3cae
Jack Hsieh <jackellishsieh@gmail.com>
2025-06-02 07:09:10 +0000

    Update sft sh

--- R3_others/scripts/step1_supervised_finetuning/R3_sft.sh
+++ R3_others/scripts/step1_supervised_finetuning/R3_sft.sh
@@ -10,17 +10,21 @@ ZERO_STAGE=2
 
 # for math datasets, train_epochs=2
 # for other datasets, train_epochs=5
-num_train_epochs=2
+num_train_epochs=10
 
 # they use a model with 7B parameters and hidden size 4096 + learning rate 2e-5
 # we use a model with 0.5B parameters and hidden size 896 + learning rate 1e-4
-learning_rate=5e-6
+learning_rate=1e-6
 
 model_name_or_path="Qwen/Qwen2-0.5B-Instruct"
 data_path="../../data/gsm8k_cot/gsm8k_nl_train_example.json"
 output_base="/home/ubuntu/LLM-R4/output_models/sft_gsm8k_cot/"
 output_dir=${output_base}lr${learning_rate}_ep${num_train_epochs}/
 data_output_path=${output_base}
+
+eval_input_path="${HOME}/LLM-R4/R3_others/data/gsm8k_cot/gsm8k_test.json"
+eval_output_path="${HOME}/LLM-R4/eval_results/sft_model_eval.json"
+src_name="gsm8k"
 
 # wandb information
 wandb_log="True"
@@ -55,8 +59,13 @@ main.py \
     --num_train_epochs ${num_train_epochs} \
     --deepspeed \
     --output_dir ${output_dir} \
+    --eval_input_path ${eval_input_path} \
+    --eval_output_path ${eval_output_path} \
+    --src_name $src_name \
+    --max_gen_length 700 \
     --wandb_log "${wandb_log}" \
     --wandb_project "${wandb_project}" \
     --wandb_entity "${wandb_entity}" \
     --wandb_run_name "${wandb_run_name}" \
+    --verbose \
     > /home/ubuntu/LLM-R4/log_dir/sft_gsm8k_cot.log 2>&1
